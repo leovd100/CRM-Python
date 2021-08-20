a5078b36f8afe21958482e5a44e41a4cbcf3486c
Leonardo <demetrio.vd@hotmail.com>
2021-08-20 12:03:07 -0300

    Added delete and update methods

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Customer
 from .forms import CustomerForm
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
 class CustomerListView(ListView):
 # Create your views here.
@@ -16,3 +18,28 @@ class CustomerCreateView(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("customer:customer-list")
+
+class CustomerUpdateView(UpdateView):
+    template_name = "customer/customer.html"
+    form_class = CustomerForm
+
+    def get_object(self):
+        id = self.kwargs.get("id")
+        return get_object_or_404(Customer, id=id)
+
+    def form_invalid(sefl, form):
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse("customer:customer-list")
+
+class CustomerDeleteView (DeleteView):
+    def get_object(self):
+        id = self.kwargs.get("id")
+        return get_object_or_404(Customer, id=id)
+
+    def get_success_url(self):
+        return reverse("customer:customer-list")
